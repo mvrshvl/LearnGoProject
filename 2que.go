47f0e770c886b6667c4aa1a6124276c19e2d00b9
Sergey <msgcloud2@gmail.com>
2020-07-14 01:26:16 +0400

    1 question

--- 2que.go
+++ 2que.go
@@ -12,6 +12,25 @@ func que12(in []int) (out []int) {
 }
 
 func que13(in []int) (out []int) {
-	in = append(in, 5)
-	return in
+	out = append(out, 5)
+	out = append(out, in[:]...)
+	return
+}
+
+func que14(in []int) (out []int, last int) {
+	last = in[len(in)-1]
+	out = append(in[0 : len(in)-1])
+	return
+}
+
+func que15(in []int) (out []int, first int) {
+	first = in[0]
+	out = append(in[1:])
+	return
+}
+
+func que16(in []int, i_in int) (out []int, i_out int) {
+	i_out = in[i_in]
+	out = append(in[0:i_in], in[i_in+1:]...)
+	return
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	/*--------------ДЗ1----------------------------------*/
 	//1.1
@@ -13,7 +15,27 @@ func main() {
 	//for i := 0; i < 10; i++ {
 	//	fmt.Println(f())
 	//}
-	//fmt.Print(que11([]int{1,2,3,4,5}))
 	/*---------------------------------------------------*/
-
+	//2.1.1
+	fmt.Println(que11([]int{1, 2, 3, 4, 5}))
+	//2.1.2
+	fmt.Println(que12([]int{1, 2, 3, 4, 5}))
+	//2.1.3
+	fmt.Println(que13([]int{1, 2, 3, 4, 5}))
+	//2.1.4
+	var (
+		a []int = []int{1, 2, 3, 4, 5}
+		b int
+	)
+	a, b = que14(a)
+	fmt.Println(a, b)
+	//2.1.5
+	a = []int{1, 2, 3, 4, 5}
+	a, b = que15(a)
+	fmt.Println(a, b)
+	//2.1.6
+	// fmt.Scan(&i) Нужен ли?
+	a = []int{1, 2, 3, 4, 5}
+	a, b = que16(a, 1)
+	fmt.Println(a, b)
 }
