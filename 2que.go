e5e2a96de5e30db720a09a400419911effb3f07c
Sergey <msgcloud2@gmail.com>
2020-07-14 00:39:45 +0400

    1 question

--- 2que.go
+++ 2que.go
@@ -1,1 +1,12 @@
 package main
+
+func que11(in []int) (out []int) {
+	for i := 0; i < len(in); i++ {
+		in[i]++
+	}
+	return in
+}
+func que12(in []int) (out []int) {
+	in = append(in, 5)
+	return in
+}
